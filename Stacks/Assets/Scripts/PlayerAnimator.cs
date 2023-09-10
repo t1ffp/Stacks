89c26999e3591cafcdfad19236e331ce89693aeb
t1ffp <pengt796@newschool.edu>
2023-09-09 23:41:10 -0400

    commit

---
+++ Stacks/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace TarodevController {
+    /// <summary>
+    /// This is a pretty filthy script. I was just arbitrarily adding to it as I went.
+    /// You won't find any programming prowess here.
+    /// This is a supplementary script to help with effects and animation. Basically a juice factory.
+    /// </summary>
+    public class PlayerAnimator : MonoBehaviour {
+        [SerializeField] private Animator _anim;
+        [SerializeField] private AudioSource _source;
+        [SerializeField] private LayerMask _groundMask;
+
+
+        private IPlayerController _player;
+        private bool _playerGrounded;
+        private Vector2 _movement;
+
+        void Awake() => _player = GetComponentInParent<IPlayerController>();
+
+        void Update()
+        {
+            if (_player == null) return;
+
+            // Flip the sprite
+            if (_player.Input.X != 0) transform.localScale = new Vector3(_player.Input.X > 0 ? 1 : -1, 1, 1);
+
+
+        }
+    
+    }
+}
